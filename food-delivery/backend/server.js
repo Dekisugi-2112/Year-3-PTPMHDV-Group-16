0bd7fa58f6e814f4b19c91abdb194ff22e2f2e12
ntv2609 <ntv2609@gmail.com>
2025-11-26 23:48:36 +0700

    vinh-setup xong mongodb-4:28:12

--- food-delivery/backend/server.js
+++ food-delivery/backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connect } from "mongoose"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 
@@ -17,6 +18,9 @@ app.use(cors())
 connectDB();
 
 //api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+
 
 app.get("/",(req, res)=>{
     res.send("API Working")
